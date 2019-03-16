4994c9fc320c0025cc721ecb07f5ccbd8c16572c
Michael Odumosu <michaelodumosu57@gmail.com>
2019-03-16 19:20:43 -0400

    TDD failed, it did not push to the array ringUp receives an integer it should hold it

--- test/test.js
+++ test/test.js
@@ -1,19 +1,32 @@
-  class CashRegister {
+class CashRegister {
 
 
-      ringUp(){
-      	return 'true'
-      }
+	ringUp(){
+		console.log(arguments)
+		return 'true'
+	}
 
 
-  }      
+}      
+var cashRA = []
+var a; //used for the inital length
+var b; //used for the final length
+
 
 const assert = require('assert'); 
 const register = new CashRegister();
 describe('CashRegister', function() {
   describe('#ringUp()', function() {
-    it('should return true', function() {
-      assert.equal(register.ringUp(), 'true');
+    it('should push the integer to an array cashRA', function() {
+    	a =  cashRA.length
+    	register.ringUp(2)
+    	assert.equal(   cashRA.length, a+1 );
     });
   });
 });
+
+
+
+/*assumptions
+	0. the ringUp function requires an array
+*/
